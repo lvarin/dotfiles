78f523fbf353d948b8773cfff14bae4db8218737
alvaro.gonzalez <alvaro.gonzalez@csc.fi>
2025-11-10 08:12:51 +0200

    Few changes, minor

---
+++ bin/record-screen.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+#
+
+ACTION=$1
+ACTION=${ACTION,,}
+
+if [[ -z "$ACTION" ]]; then
+  echo "Use: $0 <start/stop>"
+fi
+
+if [[ "start" == "$ACTION" ]]; then
+  # Pick mode: full screen or region
+  mode=$(yad --list \
+    --title="Screen Recording" \
+    --button=gtk-ok:0 \
+    --button=gtk-cancel:1 \
+    --column="Select Mode" \
+    --height=200 --width=300 \
+    "Full Monitor" \
+    "Select Region" |
+    tr -d '|')
+
+  outfile="$HOME/Pictures/record-$(date +"%Y-%m-%d--%H-%M-%S").mp4"
+
+  if [[ "$mode" == "Full Monitor" ]]; then
+    # Select monitor
+    monitor=$(swaymsg -t get_outputs | jq -r '.[].name' |
+      yad --list \
+        --title="Select Monitor" \
+        --button=gtk-ok:0 \
+        --button=gtk-cancel:1 \
+        --column="Monitor" \
+        --height=200 --width=300 |
+      tr -d '|')
+
+    [ -z "$monitor" ] && exit 0
+
+    # notify-send "📺 Recording monitor: $monitor"
+    wf-recorder -o "$monitor" -a -f "$outfile"
+
+  elif [[ "$mode" == "Select Region" ]]; then
+    notify-send "📐 Select region to record"
+
+    region=$(slurp)
+    [ -z "$region" ] && exit 0
+
+    notify-send "📹 Recording region..."
+    wf-recorder -g "$region" -a -f "$outfile"
+  else
+    notify-send "Recording aborted"
+  fi
+
+elif [[ "stop" == "$ACTION" ]]; then
+  pkill -SIGINT wf-recorder && notify-send "🟥 Recording stopped"
+fi
