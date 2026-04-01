ab09b16a0266b67045a3c1f025803c77a312392b
alvaro.gonzalez <alvaro.gonzalez@csc.fi>
2026-04-01 16:12:17 +0300

    Sway minor changes

---
+++ roles/nvim/files/lua/plugins/bufferline.lua
@@ -0,0 +1,12 @@
+return {
+  "akinsho/bufferline.nvim",
+  version = "*",
+  dependencies = 'nvim-tree/nvim-web-devicons',
+  config = function()
+      require("bufferline").setup({})
+
+      keymap.set("n", "<Tab>", "<cmd>BufferLineCycleNext<CR>", { silent = true })
+      keymap.set("n", "<S-Tab>", "<cmd>BufferLineCyclePrev<CR>", { silent = true })
+      keymap.set("n", "<leader>x", "<cmd>bdelete<CR>", { silent = true })
+  end,
+}
