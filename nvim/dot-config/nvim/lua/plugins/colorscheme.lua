ddf42bda7394cb02884f33483c8fd95bff4a6897
alvaro.gonzalez <alvaro.gonzalez@csc.fi>
2026-07-23 15:10:13 +0300

    Change formatting to maker it more compliant

--- nvim/dot-config/nvim/lua/plugins/colorscheme.lua
+++ nvim/dot-config/nvim/lua/plugins/colorscheme.lua
@@ -1,8 +1,10 @@
 return {
-  "ellisonleao/gruvbox.nvim",
-  priority = 1000,
-  config = function()
-    vim.o.background = "dark" -- or "light" if you prefer
-    vim.cmd.colorscheme("gruvbox")
-  end,
+  {
+    "ellisonleao/gruvbox.nvim",
+    priority = 1000,
+    config = function()
+      vim.o.background = "dark" -- or "light"
+      vim.cmd("colorscheme gruvbox")
+    end,
+  },
 }
