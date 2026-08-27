ef367db09ac11dcaba5f5219e0758224a5224976
Álvaro González Álvarez <alvaro.gonzalez@csc.fi>
2026-08-27 16:14:54 +0300

    No more pactl say hello to wpctl

--- scripts/dot-local/bin/volume.sh
+++ scripts/dot-local/bin/volume.sh
@@ -1,40 +1,39 @@
 #!/bin/bash
-#
 
-function isMuted() {
-  SINK=$1
+ACTION="$1"
 
-  [[ "$(pactl get-sink-mute $SINK | awk '{print $2}')" == 'yes' ]] && return 0
-
-  return 1
-}
-
-ACTION=$1
-
-if [ -z "$ACTION" ];
-then
-  echo "Use: $0 <+5%|-5%|...>" >&2
-  exit 1
+if [[ -z "$ACTION" ]]; then
+    echo "Use: $0 <+5%|-5%|toggle>" >&2
+    exit 1
 fi
 
-DEFAULT_SINK=$(pactl list sinks | grep ^Sink | head -1 | awk -F\# '{print $2}')
+SINK="@DEFAULT_AUDIO_SINK@"
 
-pactl set-sink-volume $DEFAULT_SINK "$ACTION"
+case "$ACTION" in
+    toggle)
+        wpctl set-mute "$SINK" toggle
+        ;;
 
-VOL=$(pactl list sinks | egrep '[[:blank:]]Volume:' -w | awk '{print $5}' | head -1)
-for s in $(pactl list sinks | grep Sink | awk -F\# '{print $2}');
-do
-  if [[ "$ACTION" == "toggle" ]];
-  then
-    [[ $s -ne 0 ]] && pactl set-sink-mute $s toggle
-  else
-    [[ $s -ne 0 ]] && pactl set-sink-volume $s $VOL
-  fi
-done
+    +*)
+        wpctl set-volume "$SINK" "${ACTION#+}+"
+        ;;
 
-if isMuted $DEFAULT_SINK;
-then
-  notify-send -t 1000 -a 'wp-vol' "🔇 MUTED"
+    -*)
+        wpctl set-volume "$SINK" "${ACTION#-}-"
+        ;;
+
+    *)
+        wpctl set-volume "$SINK" "$ACTION"
+        ;;
+esac
+
+VOL=$(wpctl get-volume "$SINK" | awk '{print $2" * 100"}' | bc -l )
+
+if [[ "$INFO" == *"[MUTED]"* ]]; then
+    notify-send -t 1000 -a 'wp-vol' "🔇 MUTED"
 else
-  notify-send -t 1000 -a 'wp-vol' -h int:value:$VOL "${VOL}" -i /usr/share/icons/Paper/48x48/apps/volume-knob.png
+    notify-send -t 1000 -a 'wp-vol' \
+        -h "int:value:$VOL" \
+        "${VOL}%" \
+        -i /usr/share/icons/Paper/48x48/apps/volume-knob.png
 fi
